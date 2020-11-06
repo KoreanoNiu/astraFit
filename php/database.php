bbb9619ee38f5dda64aac1ae5fb341cb960b5af4
KoreanoNiu <63904140+KoreanoNiu@users.noreply.github.com>
2020-11-05 21:03:41 -0600

    Update database.php

--- php/database.php
+++ php/database.php
@@ -2,10 +2,10 @@
 
     class Conexion{
         public static function Conectar(){
-            define('servidor', 'bynxgl7z5woudf4e3x46-mysql.services.clever-cloud.com');
-            define('nombre_bd', 'bynxgl7z5woudf4e3x46');
-            define('usuario', 'uuxgfurh3jclp53w');
-            define('password', 'sDjvkuahGJPgJYpJB7L2');
+            define('servidor', 'localhost');
+            define('nombre_bd', 'astrafitdb');
+            define('usuario', 'root');
+            define('password', '');
 
             try {
                 $conexion = new PDO("mysql:host=" . servidor ."; dbname=" . nombre_bd, usuario, password);
