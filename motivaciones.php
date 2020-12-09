3b0335e2c9959c3aafc20d977e9bfd258836a483
manyquesada <64049630+manyquesada@users.noreply.github.com>
2020-12-09 13:08:38 -0600

    Update motivaciones.php

--- motivaciones.php
+++ motivaciones.php
@@ -1,13 +1,47 @@
 <?php 
-
     session_start();
 
-    if(isset($_SESSION['id_usuario']) && $_SESSION['id_usuario'] != ''){
+    if(isset($_SESSION['idUsuario']) && $_SESSION['idUsuario'] != ''){
+        include_once('php/database.php');
+        $conexion = Conexion::Conectar();
+        $funcionesDB = new funcionesDB;
+    
+    
+        $data = $funcionesDB->obtenerDatosCompletos($conexion, $_SESSION['idUsuario']);
+
+        $rolUsuario = $data[0]['tipoUsuario'];
+        $unidadAltura = $data[0]['unidadAltura'];
+        $unidadPeso = $data[0]['unidadPeso'];
+        $edad = $data[0]['edad'];
+        $peso = $data[0]['peso'];
+        $altura = $data[0]['altura'];
+        $objetivo = $data[0]['objetivo'];
+        $nivelEntrenamiento = $data[0]['nivel'];
+        $lesiones = $data[0]['lesiones'];
+        $porcentajeGrasa = $data[0]['porcentajeGrasa'];
+        $tipoDieta = $data[0]['tipoDieta'];
+        $tipoFormula = $data[0]['tipoFormula'];
+        $idCoachNutriologo = $data[0]['idCoachNutriologo'];
+        $idCoachEntrenador = $data[0]['idCoachEntrenador'];
+
+        if ($altura != null && $peso != null && $altura != '' && $peso != '') {
+            $IMC = number_format($peso / ($altura  / 100 * $altura / 100), 2);
+        }
+
+        if (!isset($_SESSION['access_tokenStrava'])) {
+            require 'php/strava-init.php';
+            define('redirect_url', 'http://localhost/astraFit/php/update-strava.php');
+            
+            //Strava Api
+            $api = new StravaApi(client_id, client_secret);
+            
+            $urlAuthStrava = $api->authenticationUrl(redirect_url, approvalPrompt, scope);
+        }
+
     }else{
         header('location: login.php');
     }
 ?>
-
 <!DOCTYPE html>
 <html lang = es>
 <html>
@@ -17,49 +51,55 @@
         <link rel = "stylesheet" href = "src/css/styles.css" type ="TEXT/CSS">
         <link rel = "stylesheet" href = "src/css/stails.css" type ="TEXT/CSS">
         <link rel = "stylesheet" href = "src/css/motivaciones.css" type ="TEXT/CSS">
-        <title>Motivacion </title>
+        <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" href="src/img/logo.png" sizes="64x64">
+        <title>Motivacion  </title>
     </head>
 
-    <body>
-        <header>
-            <nav class="fixed closed">
-                <a href = "" class = "brand-logo center"><img src="src/img/logo.png" alt=""></a>
-                <div class="sidenav-trigger hide show-on-small-and-down right">
-                    <button class="menu" onclick="this.parentNode.parentNode.classList.toggle('opened');this.parentNode.parentNode.classList.toggle('closed');this.setAttribute('aria-expanded', this.classList.contains('opened'))" aria-label="Main Menu">
-                        <svg width="40" height="40" viewBox="0 0 100 100">
-                          <path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
-                          <path class="line line2" d="M 20,50 H 80" />
-                          <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
-                        </svg>
-                      </button>
+<body>
+<header class="right main-header transparent">
+            <div class="full-menu">
+                <ul class="">
+                    <li><a href="index.php">INICIO</a></li>
+                    <li><a href="entrenamiento2.php">ENTRENAMIENTO</a></li>
+                    <li><a href="calculadora.php" >CALCULADORA</a></li>
+                    <li><a href="motivaciones.php" >MOTIVACION</a></li>
+                    <?php 
+                        if(isset($_SESSION['idUsuario'])){
+                            echo '<li><a href="cerrarSesion.php">CERRAR SESIÓN</a></li>';
+                        }else{
+                            echo '<li><a href="login.php">INICIAR SESIÓN</a></li>';
+                        }
+                    ?>
+                </ul>
+            </div>
+            <nav>
+                <div class="trigger hide show-on-small-and-down">
+                    <svg class="bars" viewBox="0 0 100 100" onclick="this.classList.toggle('active');document.querySelector('.full-menu').classList.toggle('opened');">
+                      <path class="line top" d="m 30,33 h 40 c 13.100415,0 14.380204,31.80258 6.899646,33.421777 -24.612039,5.327373 9.016154,-52.337577 -12.75751,-30.563913 l -28.284272,28.284272"></path>
+                      <path class="line middle" d="m 70,50 c 0,0 -32.213436,0 -40,0 -7.786564,0 -6.428571,-4.640244 -6.428571,-8.571429 0,-5.895471 6.073743,-11.783399 12.286435,-5.570707 6.212692,6.212692 28.284272,28.284272 28.284272,28.284272"></path>
+                      <path class="line bottom" d="m 69.575405,67.073826 h -40 c -13.100415,0 -14.380204,-31.80258 -6.899646,-33.421777 24.612039,-5.327373 -9.016154,52.337577 12.75751,30.563913 l 28.284272,-28.284272"></path>
+                    </svg>
+
                 </div>
-                <div class="sidenav-trigger left">
-                    <!--Aqui iria el icono del carrito xd-->
-                </div>
-                <!--ESta cerrado por el hide-->
-                <aside class="show">
-                    <div>
-                        <ul>
-                            <li><a href="entrenamiento.html">Entrenamiento</a></li>
-                            <li><a href="">Nutrición</a></li>
-                            <li><a href="">Nosotros</a></li>
-                            <li><a href="">Contacto</a></li>
-                            <li><a href="calculadora.html">Calculadora</a></li>
-                        </ul>
-                    </div>
-                    <div>
-                    </div>
-                </aside>
-                <ul class="hide-on-small-and-down">
-                    <li><a href="entrenamiento.html">Entrenamiento</a></li>
-                    <li><a href="">Nutrición</a></li>
-                    <li><a href="">Nosotros</a></li>
-                    <li><a href="">Contacto</a></li>
-                    <li><a href="calculadora.html">Calculadora</a></li>
+
+                <ul class="hide-on-small-and-down horizontal">
+                    <li><a href="index.php">INICIO</a></li>
+                    <li><a href="entrenamiento2.php">ENTRENAMIENTO</a></li>
+                    <li><a href="calculadora.php" >CALCULADORA</a></li>
+                    <li><a href="motivaciones.php" >MOTIVACION</a></li>
+                    <?php 
+                        if(isset($_SESSION['idUsuario'])){
+                            echo '<li><a href="cerrarSesion.php">CERRAR SESIÓN</a></li>';
+                        }else{
+                            echo '<li><a href="login.php">INICIAR SESIÓN</a></li>';
+                        }
+                    ?>
                 </ul>
             </nav>
         </header>
-        <body>
             
           <!--PORTADA -->
 
